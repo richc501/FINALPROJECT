8a9c7887081a22b9ff1e71d59016009eec8051fe
Destroierrr9 <Destroierrr9@Destroierrr9-HP>
2014-12-03 17:42:08 -0500

    FINAL PROJECT

--- CSC 201 FINAL PROJECT 3/src/login/addDefaultUser.java
+++ CSC 201 FINAL PROJECT 3/src/login/addDefaultUser.java
@@ -3,7 +3,7 @@ package login;
 public class addDefaultUser {
 	public static void main(String[]args)
 	{
+		//sets default user for first launch
 		userAndPasswordSaver admin = new userAndPasswordSaver("Admin",1234);
-		
 	}
 }
