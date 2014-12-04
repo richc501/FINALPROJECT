eafda7715745138dfd33e8185dd3bb4ad18ac9f5
Destroierrr9 <Destroierrr9@Destroierrr9-HP>
2014-12-04 13:57:34 -0500

    FINAL PROJECT

--- CSC 201 FINAL PROJECT 3/src/login/runLogin.java
+++ CSC 201 FINAL PROJECT 3/src/login/runLogin.java
@@ -2,8 +2,6 @@ package login;
 //Richard Cunningham
 //Admin: Pasword- 1234
 //test1: PAsword- 5678
-//FINISHED AT 2PM 12/4/2014 
-//Now time to play some Firefall :P
 public class runLogin {
 	
 	public static void main(String[] args) {
